6d83f0ae9ee145765827015e0ce772920ce6694b
GhaSilva <ghabriel-silva@outlook.com>
2021-07-20 15:40:32 -0300

    Adicionando items na tabela

--- js/principal.js
+++ js/principal.js
@@ -43,5 +43,41 @@ if(alturaIstrue && pesoIsTrue){
 }
 if(!alturaIstrue || !pesoIsTrue){
     tdImc.textContent = "Dados Inválidos";
+    }
 }
-};
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    var form = document.querySelector("#adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+ 
+
+    pacienteTr.appendChild(nomeTd)
+    pacienteTr.appendChild(pesoTd)
+    pacienteTr.appendChild(alturaTd)
+    pacienteTr.appendChild(gorduraTd)
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
+    console.log(pacienteTr);
+
+});
