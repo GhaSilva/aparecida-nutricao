f717d10f9fd7fe673542fef18f1618bf94505704
GhaSilva <ghabriel-silva@outlook.com>
2021-07-22 18:28:28 -0300

    Filtrando Resultados com regex

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -2,8 +2,17 @@ var pacientes = document.querySelectorAll(".paciente");
 
 var tabela = document.querySelector("table");
 tabela.addEventListener("dblclick", function(event){
-    event.target.parentNode.remove();
-})
+
+        console.log(pacientes)
+
+        event.target.parentNode.classList.add("fadeOut");
+
+        setTimeout(function(){
+            event.target.parentNode.remove();
+        }, 500);
+
+        }); 
+    
 
 //pacientes.forEach(function(paciente){
 //    paciente.addEventListener("dblclick", function(){
