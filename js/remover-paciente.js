db246bd539b1ef9a8f46ee82b55847e6af7e2009
GhaSilva <ghabriel-silva@outlook.com>
2021-07-21 23:54:39 -0300

    Removendo Pacientes

---
+++ js/remover-paciente.js
@@ -0,0 +1,12 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+var tabela = document.querySelector("table");
+tabela.addEventListener("dblclick", function(event){
+    event.target.parentNode.remove();
+})
+
+//pacientes.forEach(function(paciente){
+//    paciente.addEventListener("dblclick", function(){
+//        this.remove(paciente);
+//    })
+//});
