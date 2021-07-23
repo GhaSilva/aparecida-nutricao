8913400021e85e4736d5768dbc08ac596ca651eb
GhaSilva <ghabriel-silva@outlook.com>
2021-07-23 07:22:33 -0300

    Capturando Erros da Requisição

--- js/buscar-paciente.js
+++ js/buscar-paciente.js
@@ -6,19 +6,21 @@ botaoAdicionar.addEventListener("click", function(){
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
     xhr.send();
     xhr.addEventListener("load", function(){
-        var resposta = xhr.responseText
-        console.log(resposta);
-        var pacientes = JSON.parse(resposta);
-        console.log(pacientes);
-
-        for(var i = 0; i < pacientes.length; i ++){
-            adicionaPacienteNaTabela(pacientes[i])
-        }
-            
-      
-
-        adicionaPacienteNaTabela(paciente)
+        if(xhr.status == 200){
+            var resposta = xhr.responseText
+            var pacientes = JSON.parse(resposta);
+            var erroAjax = document.querySelector("#erro-ajax");
+            erroAjax.classList.add("invisivel")
+    
+            for(var i = 0; i < pacientes.length; i ++){
+                adicionaPacienteNaTabela(pacientes[i])
+            }
+        }else{
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            var erroAjax = document.querySelector("#erro-ajax");
+            erroAjax.classList.remove("invisivel")
+        }  
     })
-
 });
 
